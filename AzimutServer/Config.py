28584103987b8a89828c02927f69a00227ab8f9f
Raph Bloch <raphaelbloch873@gmail.com>
2025-05-29 14:35:30 +0300

    Use class for settings in server

--- AzimutServer/Config.py
+++ AzimutServer/Config.py
@@ -1,4 +1,18 @@
+import os
 
+from dotenv import load_dotenv
+
+from pydantic_settings import BaseSettings
+
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASSWORD: str = os.getenv("DB_PWD")
+    DB_NAME : str = os.getenv("DB_NAME")
+    DB_HOST : str  = os.getenv("DB_HOST", "localhost")
+    DB_PORT: str = "5432"
 
 
 
--- AzimutServer/DAL/db.py
+++ AzimutServer/DAL/db.py
@@ -4,17 +4,16 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+from Config import Settings
+
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PWD")
-DB_NAME = os.getenv("DB_NAME")
-DB_HOST = os.getenv("DB_HOST", "localhost")
 
 
+settings = Settings()
 
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+DATABASE_URL = f"postgresql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:5432/{settings.DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
